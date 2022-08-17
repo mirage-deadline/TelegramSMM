da1e4cb4919b45d190cdc5a8eaba52727f0e7409
Dmitry <dmitrio.skripka@gmail.com>
2022-08-18 00:01:37 +0300

    uncomment last row

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ async def main():
 if __name__ == '__main__':
     remove_old_sessions()
     loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
+    loop.run_until_complete(main())
+    
